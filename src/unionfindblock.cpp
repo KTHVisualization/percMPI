0652af1daf8205e73611303927891f811032cc3e
Anke <ankef@kth.se>
2018-12-04 15:49:26 +0100

    :scroll: UnionFindBlock now abstract, sketching LocalBlock

--- src/unionfindblock.cpp
+++ src/unionfindblock.cpp
@@ -3,13 +3,54 @@
 
 namespace perc {
 
-ClusterID* UnionFindBlock::findClusterID(const vec3i& idx, vec3i& lastClusterID) {
-    // One might want to do this more cleverly, especialy in the sheet tree.
-    for (UnionFindSubBlock* sub : SubBlocks)
-        if (sub->contains(idx)) return sub->findClusterID(idx, lastClusterID);
+LocalBlock::LocalBlock(const vec3i& blockSize, const vec3i& blockOffset, const vec3i& totalSize) {
+    // TODO: Load subblocks into data
+    // (const vec3i& size, const vec3i& offset, const vec3i& total,
+    //                             UnionFindBlock& parent, const Watershedder& watershedder);
+    vec3i min = blockOffset;
+    vec3i max = blockOffset + blockSize;
+    for (ind d = 0; d < 3; ++d) {
+        min[d] = min[d] > 0 ? min[d]++ : 0;
+        max[d] = max[d] == totalSize[d] ? max[d] - 2 : totalSize[d];
+    }
+}
 
-    assert(false && "Can not find block containing this idx.");
+void LocalBlock::doWatershed(double maxVal) {
+    // TODO: for each subblock....
+}
+ClusterID* LocalBlock::findClusterID(const vec3i& idx, vec3i& lastClusterID) {
+    // TODO: Do this more cleverly, and for all types of subblocks:
+    /*
+    ClusterID* UnionFindBlock::findClusterID(const vec3i& idx, vec3i& lastClusterID) {
+        // One might want to do this more cleverly, especialy in the sheet tree.
+        for (UnionFindSubBlock* sub : SubBlocks)
+            if (sub->contains(idx)) return sub->findClusterID(idx, lastClusterID);
+
+        assert(false && "Can not find block containing this idx.");
+        return nullptr;
+    }
+    */
     return nullptr;
+}
+
+// Sketch.
+void LocalBlock::receiveData() {
+    /*  TODO: Receive merge sets
+     *      Merge locally
+     *
+     *  TODO: Receive numNewLOGs
+     *      Do LOGs.addCluster() that often
+     *      Go through CommPLOGS:
+     *          Add the representative to the respective LOG
+     */
+}
+void LocalBlock::sendData() {
+    /*  TODO: Fill CommPLOGs:
+     *      Move clusters into CommPLOGs
+     *      Remove from LOL list, clear RefPLOGs
+     *      Send over LOL TotalVolume and number of clusters
+     *      Send over CommPLOGs
+     */
 }
 
 }  // namespace perc
--- src/unionfindblock.h
+++ src/unionfindblock.h
@@ -2,17 +2,39 @@
 #include <vector>
 #include "vec.h"
 #include "handles.h"
+#include "clusterlist.h"
+#include "clusterlistrecording.h"
 
 namespace perc {
 
-class UnionFindSubBlock;
-
 class UnionFindBlock {
 public:
-    ClusterID* findClusterID(const vec3i& idx, vec3i& lastClusterID);
-
-protected:
-    std::vector<UnionFindSubBlock*> SubBlocks;
+    virtual void doWatershed(double maxVal) = 0;
+    virtual ClusterID* findClusterID(const vec3i& idx, vec3i& lastClusterID) = 0;
+    virtual void receiveData() = 0;
+    virtual void sendData() = 0;
 };
+
+class LocalBlock : public UnionFindBlock {
+public:
+    LocalBlock(const vec3i& blockSize, const vec3i& blockOffset, const vec3i& totalSize);
+
+    virtual void doWatershed(double maxVal) override;
+    virtual ClusterID* findClusterID(const vec3i& idx, vec3i& lastClusterID) override;
+
+    // Sketch.
+    virtual void receiveData() override;
+    virtual void sendData() override;
+
+private:
+    // Local representations of local clusters.
+    ClusterList LOLs;
+    // Local representations of global clusters.
+    ClusterListRecording LOGs;
+    // Potential LOGs: LOLs that touch the boundary.
+    std::vector<ClusterID> RefPLOGs;
+    // List of PLOGS that will be used for sending and receiving data.
+    std::vector<Cluster> CommPLOGs;
+};  // namespace perc
 
 }  // namespace perc

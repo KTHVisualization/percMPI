951a1668c3ee87005cd187aa93f14c8805eff30d
Wiebke <wiebke.koepp@googlemail.com>
2019-01-14 19:16:05 +0100

    :envelope: Use .data() for getting vector data pointer

--- src/mpicommuncation.h
+++ src/mpicommuncation.h
@@ -41,26 +41,28 @@ public:
 
 template <typename T>
 int MPICommunication::SendVector(std::vector<T>& vector, int dest, int tag, MPI_Comm comm) {
-    return MPI_Send(&vector[0], vector.size() * sizeof(T), MPI_BYTE, dest, tag, comm);
+    return MPI_Send(vector.data(), vector.size() * sizeof(T), MPI_BYTE, dest, tag, comm);
 }
 
 template <typename T>
 int MPICommunication::IsendVector(std::vector<T>& vector, int dest, int tag, MPI_Comm comm,
                                   MPI_Request* request) {
-    return MPI_Isend(&vector[0], vector.size() * sizeof(T), MPI_BYTE, dest, tag, comm, request);
+    // Sends an empty message for size equal to zero, bugger does not matter send
+    return MPI_Isend(vector.data(), vector.size() * sizeof(T), MPI_BYTE, dest, tag, comm, request);
 }
 
 template <typename T>
 int MPICommunication::RecvVectorUknownSize(std::vector<T>& vector, int src, int tag, MPI_Comm comm,
                                            MPI_Status* status) {
-    // Resize if we cannot hold the data to be received
+
     int err = MPI_Probe(src, tag, comm, status);
     if (err != MPI_SUCCESS) return err;
     int messageSize;
     MPI_Get_count(status, MPI_BYTE, &messageSize);
     ind vectorSize = messageSize / sizeof(T);
-    if (vector.capacity() != vectorSize) vector.resize(vectorSize);
-    return MPI_Recv(&vector[0], messageSize, MPI_BYTE, src, tag, comm, MPI_STATUS_IGNORE);
+    // Resize if we cannot hold the data to be received
+    if (vector.size() != vectorSize) vector.resize(vectorSize);
+    return MPI_Recv(vector.data(), messageSize, MPI_BYTE, src, tag, comm, MPI_STATUS_IGNORE);
 }
 
 }  // namespace perc
